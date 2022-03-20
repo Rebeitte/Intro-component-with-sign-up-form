0a2a7d6e640a2b205bd50d4616b3852a761043d0
jesusrebeitte <jesusrebeitte@gmail.com>
2022-03-19 20:06:37 -0400

    Eventos en Javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,10 @@
-console.log("Alerta")
+let submit = document.querySelector(".submit"); 
+submit.addEventListener('click', function enviar(enviar) {
+    let error = document.querySelector(".error_name");
+    error.classList.add("error");
+    let alert = document.querySelector(".alert_name");
+    alert.classList.add("display");
+
+    enviar.preventDefault();
+})
+
