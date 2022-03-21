f14c26209e91c1c83822d41033323e18ef3d69a3
jesusrebeitte <jesusrebeitte@gmail.com>
2022-03-20 21:13:16 -0400

    listo

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,35 @@
-let submit = document.querySelector(".submit"); 
-submit.addEventListener('click', function enviar(enviar) {
-    let error = document.querySelector(".error_name");
-    error.classList.add("error");
-    let alert = document.querySelector(".alert_name");
-    alert.classList.add("display");
+const data = {
+    firstname: "",
+    lastname: "",
+    email: "",
+    password: ""
+}
 
-    enviar.preventDefault();
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const mail = document.querySelector('.error_email')
+let alert = document.querySelector('.alert')
+
+firstname.addEventListener('input', alldata);
+lastname.addEventListener('input', alldata);
+email.addEventListener('input', alldata);
+password.addEventListener('input', alldata);
+
+function alldata(e) {
+    data[e.target.id] = e.target.value;
+    console.log(data)
+}
+
+const btn = document.querySelector(".submit");
+btn.addEventListener('click', function send(e) {
+        firstname.classList.add('error');
+        lastname.classList.add('error');
+        email.classList.add('alert');
+        mail.classList.add('display')
+        password.classList.add('error');
+        alert.classList.add('error');
+    e.preventDefault();
+
 })
-
